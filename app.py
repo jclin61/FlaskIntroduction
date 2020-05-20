a6438fd5534ce98ddfe48a658b1817578e766a1f
Jerry Chia Wei Lin <j13n@Jerrys-MacBook-Pro.local>
2020-05-20 04:58:46 -0700

    pybabel translation module added

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, session
+from flask_babel import Babel
+from flask_babel import gettext
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+babel = Babel(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,9 +19,52 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/language/<language>')
+def set_language(language=None):
+    session['language'] = language
+    return redirect(url_for('index'))
+
+# Use the browser's language preferences to select an available translation
+@babel.localeselector
+def get_locale():
+    # if the user has set up the language manually it will be stored in the session,
+    # so we use the locale from the user settings
+    try:
+        language = session['language']
+    except KeyError:
+        language = None
+    if language is not None:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.context_processor
+def inject_conf_var():
+    return dict(
+                AVAILABLE_LANGUAGES=app.config['LANGUAGES'],
+                CURRENT_LANGUAGE=session.get('language',request.accept_languages.best_match(app.config['LANGUAGES'])))
+
+app.jinja_env.globals['get_locale'] = get_locale
+
 
 if __name__ == "__main__":
+    app.secret_key = 'some secret key'
     app.run(debug=True)
+
+    
